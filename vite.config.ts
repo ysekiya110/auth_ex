a5c872089082b49322ac77bfed18e3848c4ea0b3
ysekiya110 <uectlab.s@gmail.com>
2023-12-11 17:19:49 +0900

    add example

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,15 @@ plugins: [react()],
       input: {
         form1: resolve(__dirname, `src/form1/index.html`),
         form2: resolve(__dirname, `src/form2/index.html`),
-        form3: resolve(__dirname, `soc/form3/index.html`)
+        form3: resolve(__dirname, `src/form3/index.html`),
+        form4: resolve(__dirname, `src/form4/index.html`),
+        example: resolve(__dirname, `src/example/index.html`)
       }
     }
+  },
+
+  server:{
+    host: "0.0.0.0"
   }
   
 })
